ee673bc31ed61a0ebcf8833a0787ac58d9213a2a
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2024-08-21 00:39:28 -0300

    a

---
+++ script.js
@@ -0,0 +1,6 @@
+function updateWindowSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    document.getElementById('width').textContent = width;
+    document.getElementById('height').textContent = height;
+}
